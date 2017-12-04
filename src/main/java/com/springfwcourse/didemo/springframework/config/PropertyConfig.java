6dd86d9b918b620bc1317fa77094182084eb71bf
yusufduyar <yusufduyar@outlook.com>
2017-12-04 14:16:28 +0300

    Used environment properties

--- src/main/java/com/springfwcourse/didemo/springframework/config/PropertyConfig.java
+++ src/main/java/com/springfwcourse/didemo/springframework/config/PropertyConfig.java
@@ -1,15 +1,20 @@
 package com.springfwcourse.didemo.springframework.config;
 
 import com.springfwcourse.didemo.springframework.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment environment;
 
     @Value("${com.username}")
     String user;
@@ -23,7 +28,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource= new FakeDataSource();
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(environment.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
 
